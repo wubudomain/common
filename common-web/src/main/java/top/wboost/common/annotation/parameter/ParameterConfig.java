50bea807ef6c8c3601c04ef3d265976c22424ac3
wboost <wboost.top@gmail.com>
2018-06-07 16:46:58 +0800

    自定义参数校验注解

--- common-web/src/main/java/top/wboost/common/annotation/parameter/ParameterConfig.java
+++ common-web/src/main/java/top/wboost/common/annotation/parameter/ParameterConfig.java
@@ -7,8 +7,8 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
- * 参数值不能为空
- * @className NotNull
+ * 参数值校验配置(需要用到参数校验注解需添加此注解)
+ * @className ParameterConfig
  * @author jwSun
  * @date 2017年9月14日 上午10:32:01
  * @version 1.0.0
